3e5bfaaebdf2e7e8f2c5755aae7dbabaf27b1891
Ingrid <ingrid.fernandes@sensedia.com>
2025-12-29 11:41:55 -0300

    feat(adapter-in): add REST endpoint to publish pedidos to kafka

---
+++ src/main/java/com/seuusuario/pedidokafka/adapters/in/rest/PedidoController.java
@@ -0,0 +1,26 @@
+package com.seuusuario.pedidokafka.adapters.in.rest;
+
+import com.seuusuario.pedidokafka.adapters.in.rest.dto.PedidoRequest;
+import com.seuusuario.pedidokafka.adapters.in.rest.mapper.PedidoMapper;
+import com.seuusuario.pedidokafka.application.usecase.EnviarPedidoUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    private final EnviarPedidoUseCase enviarPedidoUseCase;
+
+    public PedidoController(EnviarPedidoUseCase enviarPedidoUseCase) {
+        this.enviarPedidoUseCase = enviarPedidoUseCase;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void criarPedido(@RequestBody PedidoRequest request) {
+        enviarPedidoUseCase.executar(
+                PedidoMapper.toDomain(request)
+        );
+    }
+}
