23f78318dda024ed5bad93188ce4a8918450e7e5
Ingrid <ingrid.fernandes@sensedia.com>
2025-12-29 12:59:54 -0300

    feat(adapter-in): add kafka consumer with @KafkaListener

---
+++ src/main/java/com/projeto/pedidokafka/adapters/in/kafka/PedidoKafkaConsumer.java
@@ -0,0 +1,26 @@
+package com.projeto.pedidokafka.adapters.in.kafka;
+
+import com.projeto.pedidokafka.adapters.in.kafka.mapper.PedidoKafkaMapper;
+import com.projeto.pedidokafka.application.usecase.ProcessarPedidoUseCase;
+import com.projeto.pedidokafka.domain.model.Pedido;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoKafkaConsumer {
+
+    private final ProcessarPedidoUseCase processarPedidoUseCase;
+
+    public PedidoKafkaConsumer(ProcessarPedidoUseCase processarPedidoUseCase) {
+        this.processarPedidoUseCase = processarPedidoUseCase;
+    }
+
+    @KafkaListener(
+            topics = "${app.kafka.topic.pedidos}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consumir(String message) {
+        Pedido pedido = PedidoKafkaMapper.fromJson(message);
+        processarPedidoUseCase.executar(pedido);
+    }
+}
