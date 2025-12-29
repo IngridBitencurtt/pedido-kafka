f45212f53617fe4336296e767cf44dd189a08886
Ingrid <ingrid.fernandes@sensedia.com>
2025-12-29 13:02:30 -0300

    feat(application): configure use case for processing pedidos

--- src/main/java/com/projeto/pedidokafka/application/config/UseCaseConfig.java
+++ src/main/java/com/projeto/pedidokafka/application/config/UseCaseConfig.java
@@ -1,7 +1,9 @@
 package com.projeto.pedidokafka.application.config;
 
 import com.projeto.pedidokafka.application.usecase.EnviarPedidoUseCase;
+import com.projeto.pedidokafka.application.usecase.ProcessarPedidoUseCase;
 import com.projeto.pedidokafka.domain.ports.PedidoProducerPort;
+import com.projeto.pedidokafka.domain.ports.PedidoProcessorPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,16 @@ import org.springframework.context.annotation.Configuration;
 public class UseCaseConfig {
 
     @Bean
-    public EnviarPedidoUseCase enviarPedidoUseCase(PedidoProducerPort producerPort) {
+    public EnviarPedidoUseCase enviarPedidoUseCase(
+            PedidoProducerPort producerPort
+    ) {
         return new EnviarPedidoUseCase(producerPort);
+    }
+
+    @Bean
+    public ProcessarPedidoUseCase processarPedidoUseCase(
+            PedidoProcessorPort processorPort
+    ) {
+        return new ProcessarPedidoUseCase(processorPort);
     }
 }
