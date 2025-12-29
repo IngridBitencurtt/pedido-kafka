77197b73da0326569fc51a6d65ae1eb9118f435d
Ingrid <ingrid.fernandes@sensedia.com>
2025-12-29 09:57:12 -0300

    chore: initial project structure with Spring Boot 4 and Java 21

---
+++ src/main/java/com/seuusuario/pedidokafka/PedidoKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.seuusuario.pedidokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seuusuario/pedidokafka/PedidoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuusuario.pedidokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
